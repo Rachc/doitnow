a448b3054bdc325052740031a552480daa8e99d4
Rachel Curioso <rachel@rdscodes.com.br>
2018-01-17 08:27:35 -0200

    added subtask functon. Need refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,10 @@ $(document).ready(function() {
     }
   });
 
-  $('#taskInput').keypress(function(event){
+  $('#subTaskInput').keypress(function(event){
     if (event.which === 13){
       let task = $(this).val();
-      $("#taskList").append("<li><div class='task'><div class='TaskName'><input type='checkbox'>" + task + "</div><div class='taskAction'><div class='deleteTaskIcon'></div></div></div></li><ul class='subtaskList'><li></li></ul>");
+      $("#subtaskList").prepend("<li><div class='subtask'><div class='subtaskName'><input type='checkbox'>" + task + "</div><div class='subtaskAction'><div class='deleteTaskIcon'></div></div></div></li>");
       $(this).val("");
     }
   });
