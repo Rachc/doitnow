b0b7f662c2ddb7f69153dfde567e08b7e39006f1
Rachel Curioso <rachel@rdscodes.com.br>
2018-01-17 08:13:04 -0200

    added function to submit tasks

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  console.log("hello world")
+  $('#taskInput').keypress(function(event){
+    if (event.which === 13){
+      let task = $(this).val();
+      $("#taskList").append("<li><div class='task'><div class='TaskName'><input type='checkbox'>" + task + "</div><div class='taskAction'><div class='deleteTaskIcon'></div></div></div></li><ul class='subtaskList'><li></li></ul>");
+      $(this).val("");
+    }
+  });
+
+  $('#taskInput').keypress(function(event){
+    if (event.which === 13){
+      let task = $(this).val();
+      $("#taskList").append("<li><div class='task'><div class='TaskName'><input type='checkbox'>" + task + "</div><div class='taskAction'><div class='deleteTaskIcon'></div></div></div></li><ul class='subtaskList'><li></li></ul>");
+      $(this).val("");
+    }
+  });
 });
