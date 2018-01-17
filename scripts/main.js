b318f31d09d648099c4bca34df503055870c4a18
Rachel Curioso <rachel@rdscodes.com.br>
2018-01-17 08:48:33 -0200

    added validation to task and subtask inputs.

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,24 @@ $(document).ready(function() {
   $('#taskInput').keypress(function(event){
     if (event.which === 13){
       let task = $(this).val();
-      $("#taskList").append("<li><div class='task'><div class='TaskName'><input type='checkbox'>" + task + "</div><div class='taskAction'><div class='deleteTaskIcon'></div></div></div></li><ul class='subtaskList'><li></li></ul>");
-      $(this).val("");
+      if (task === ''){
+        $(this).parent().css("border", "1px solid red");
+      } else {
+        $("#taskList").append("<li><div class='task'><div class='TaskName'><input type='checkbox'>" + task + "</div><div class='taskAction'><div class='deleteTaskIcon'></div></div></div></li><ul class='subtaskList'><li></li></ul>");
+        $(this).val("");
+      }
     }
   });
 
   $('#subTaskInput').keypress(function(event){
     if (event.which === 13){
       let task = $(this).val();
-      $("#subtaskList").prepend("<li><div class='subtask'><div class='subtaskName'><input type='checkbox'>" + task + "</div><div class='subtaskAction'><div class='deleteTaskIcon'></div></div></div></li>");
-      $(this).val("");
+      if (task === ''){
+        $(this).parent().css("border", "1px solid red");
+      } else {
+        $("#subtaskList").prepend("<li><div class='subtask'><div class='subtaskName'><input type='checkbox'>" + task + "</div><div class='subtaskAction'><div class='deleteTaskIcon'></div></div></div></li>");
+        $(this).val("");
+      }
     }
   });
 });
